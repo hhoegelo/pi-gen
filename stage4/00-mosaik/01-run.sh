53d63e60d4f815ba7731e8bb85ba0c4f27fb4f66
Henry Hoegelow <henry@quelltextlabor.de>
2024-07-06 19:36:26 +0200

    convert and add sample lib

--- stage4/00-mosaik/01-run.sh
+++ stage4/00-mosaik/01-run.sh
@@ -6,9 +6,27 @@ for PACKAGE in mosaik-app; do
     wget "$BASE_URL/$PACKAGE.deb" -O "${ROOTFS_DIR}/$PACKAGE.deb"
 done
 
-git clone https://github.com/stekgreif/mosaik-sample-lib "${ROOTFS_DIR}/home/mosaik/Music/Factory Library"
+DIRECTORY="${ROOTFS_DIR}/home/mosaik/Music/Factory Library"
+
+git clone https://github.com/stekgreif/mosaik-sample-lib "${DIRECTORY}"
+rm -rf "${DIRECTORY}/.git"
+
+find "$DIRECTORY" -type f -name '*.wav' | while read -r wavfile; do
+  flacfile="${wavfile%.wav}.flac"
+  rm -rf "$flacfile"
+  flac "$wavfile" -o "$flacfile"
+
+  if [ $? -eq 0 ]; then
+    rm "$wavfile"
+  else
+    echo "Failed to convert: $wavfile"
+  fi
+done
+
 
 on_chroot << EOF
     set -x
     dpkg -i mosaik-*.deb
+    systemctl unmask mosaik.service
+    systemctl enable mosaik.service
 EOF
