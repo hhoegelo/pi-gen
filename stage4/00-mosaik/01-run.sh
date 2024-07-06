053ad8e7d339d533d0afbe28115d5ccb3c6d4aff
Henry Hoegelow <henry@quelltextlabor.de>
2024-07-06 19:11:28 +0200

    add sample lib and unclutter

--- stage4/00-mosaik/01-run.sh
+++ stage4/00-mosaik/01-run.sh
@@ -6,6 +6,8 @@ for PACKAGE in mosaik-app; do
     wget "$BASE_URL/$PACKAGE.deb" -O "${ROOTFS_DIR}/$PACKAGE.deb"
 done
 
+git clone https://github.com/stekgreif/mosaik-sample-lib "${ROOTFS_DIR}/home/mosaik/Music/Factory Library"
+
 on_chroot << EOF
     set -x
     dpkg -i mosaik-*.deb
